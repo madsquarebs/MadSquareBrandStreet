3d6585d4c1e245a4d9405fd1af6ecca85a41a4cb
madsquarebs <ssikmr4755@gmail.com>
2017-03-07 13:02:39 +0530

    Fetched and commiting again with career form

---
+++ careers_form.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width" />
+      <meta name="description" content="Advertising agencies in chennai, Digital marketing agencies in chennai, Creative advertising agencies in chennai - MadSquare Brand Street">
+      <meta name="Keywords" content="Mad square chennai,MadSquare Chennai, Ad agencies in chennai, Advertising agencies chennai, Digital marketing agencies in chennai">
+      <title>Career at MadSquare Brand Street chennai | Advertising agencies in chennai - MadSquare Brand Street - Creative Advertising Agency | Digital Marketing Agency| Branding Solutions in Chennai</title>
+       <link rel="stylesheet" href="css/components.css">
+      <link rel="stylesheet" href="css/responsee.css">
+      <link rel="stylesheet" href="owl-carousel/owl.carousel.css">
+      <link rel="stylesheet" href="owl-carousel/owl.theme.css">
+      <!-- CUSTOM STYLE -->
+      <link rel="stylesheet" href="css/template-style.css">
+      <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+      <link rel="shortcut icon" href="img/MS-logo.png">
+      <script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
+      <script type="text/javascript" src="js/jquery-ui.min.js"></script>    
+      <script type="text/javascript" src="js/modernizr.js"></script>
+      <script type="text/javascript" src="js/responsee.js"></script>
+      <script type="text/javascript" src="js/template-scripts.js"></script>
+      <script src='https://www.google.com/recaptcha/api.js'></script>
+</head>
+<body class="size-1140">
+<header> 
+         <nav>
+            <div class="line">
+               <div class="s-12 l-2">
+                  <p class="logo" alt="MadSquare chennai"><img src="img/Madsquare-wt.png"></p>
+               </div>
+               <div class="top-nav s-12 l-10">
+                  <p class="nav-text">Custom menu text</p>
+                  <!-- <ul class="right">
+                     <li class="active-item"><a href="#carousel">Home</a></li>
+                     <li><a href="#features">Features</a></li>
+                     <li><a href="#about-us">About Us</a></li>
+                     <li><a href="#our-work">Our Work</a></li>
+                     <li><a href="#services">Services</a></li>
+                     <li><a href="#contact">Contact</a></li>
+                  </ul> -->
+               </div>
+            </div>
+         </nav>
+          <div id="topbar">
+            <div class="line">
+               <!-- <div class="s-12 m-6 l-6">
+                  <p>Building brands, building future.</p>
+               </div> -->
+               <div class="s-12 m-6 l-6">
+                  <div class="social right">
+                     <a target="_blank" href="https://www.facebook.com/madsquarebrandstreet/"><i class="icon-facebook_circle"></i></a> <a><i class="icon-twitter_circle"></i></a> <a target="_blank" href="http://plus.google.com/114863483683159296719"><i class="icon-google_plus_circle"></i></a> <a><i class="icon-instagram_circle"></i></a>
+                  </div>
+               </div>
+            </div>  
+         </div>
+      </header> 
+      <div class="s-12 m-12 l-5">
+                    <h3>Contact form</h3>
+                    <form class="customform" method="POST" action="">
+                      <div class="s-12"><input id="mail" name="mail" placeholder="Your e-mail" title="Your e-mail" type="text" /></div>
+                      <div class="s-12"><input id="name" name="name" placeholder="Your name" title="Your name" type="text" /></div>
+                      <div class="s-12"><textarea id="msg" placeholder="Your message" name="msg" rows="5"></textarea></div>
+                      <div><p style="display: none;" id="ValErr"><font color="red"> Please fill the details to submit the form successfully.</font></p></div>
+                      <div><p style="display: none;" id="CaptchaErr"><font color="red"> Captcha Error: please prove you are a human.</font></p></div>
+                      <div class="s-12 m-12 l-4"><button id='submit' class="color-btn" type="submit">Submit Button</button></div>
+                       <div class="g-recaptcha" id="captcha" data-sitekey="6LciVhYUAAAAAC5-qXdxVJ0uo8qwocMaxsODJylI"></div>
+                    </form>
+                  </div>                 
+</body>
+</html>
